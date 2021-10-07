1b58936a420fd2afdf240f3b8427b767172f85cf
blenderistDev <demonx.blenderist@gmail.com>
2021-10-07 21:52:18 +0300

    init

---
+++ client.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"google.golang.org/grpc"
+	pb "proxy/client/proxy/proxy/proxy"
+	"time"
+)
+func main()  {
+	conn, err := grpc.Dial("localhost:9999", grpc.WithInsecure())
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer conn.Close()
+	c := pb.NewProxyClient(conn)
+
+
+	url := pb.Url{Value: "https://google.com"}
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+
+	defer cancel()
+
+	r, err := c.Get(ctx, &url)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(r)
+}
+
+
